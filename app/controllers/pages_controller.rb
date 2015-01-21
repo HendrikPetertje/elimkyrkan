6118644edaa9608f43b3aab62437508d53a21f78
Isak Ågren <isak.agren@hotmail.com>
2015-01-21 18:43:57 +0100

    page controller
    added pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def activities
+  end
+
+  def about
+  end
+
+  def program
+  end
+
+  def links
+  end
+
+  def contact
+  end
+
+  def prayer
+  end
+end
