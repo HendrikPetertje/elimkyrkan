b3b46164f13b54396db5c249d35607b991220293
Peter van der meulen <peter@picoreclame.nl>
2015-01-21 19:10:43 +0100

    Adding helper to get classnames running for active pages plus nice and fat css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def navigation_class_home(path)
+    'active' if current_page?(path)
+  end
+
+  def navigation_class(path)
+    'active' if request.url.include?(path)
+  end
+
 end
