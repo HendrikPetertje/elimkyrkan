78d406fefaaecc4994e4ecf70df0837531195f7e
Isak Ågren <isak.agren@hotmail.com>
2015-01-26 19:23:06 +0100

    started building a responsive layout

--- app/assets/javascripts/partials/std_script.js
+++ app/assets/javascripts/partials/std_script.js
@@ -1,1 +1,10 @@
 
+var ready;
+ready = function() {
+  var showslideshow = $("#showslideshow").content();
+
+  alert(showslideshow);
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
