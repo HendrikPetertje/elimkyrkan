8e89fded64d63dcaa70206839a9b9d3eeb242b15
isakagren <isak.agren@hotmail.com>
2015-02-11 19:28:57 +0100

    Basic sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   scope(path_names: { new: 'ny', edit: 'andra' }) do
     root 'pages#home'
 
+    get 'hem', to: 'pages#home', as: :pages_home
+
     get 'aktiviteter', to: 'pages#activities', as: :pages_activities
 
     get 'om_oss', to: 'pages#about', as: :pages_about
 
     get 'program', to: 'pages#program', as: :pages_program
 
-    get 'links', to: 'pages#links', as: :pages_links
+    get 'lankar', to: 'pages#links', as: :pages_links
 
     get 'pages/contact', to: 'pages#contact', as: :pages_contact
 
